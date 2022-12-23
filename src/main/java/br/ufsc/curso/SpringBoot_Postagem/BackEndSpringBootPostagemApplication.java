3f88cb4b156dbc381fadba3ba3f10b23f2ce727b
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-12-23 08:36:30 -0300

    Add Cross Origin permission

--- src/main/java/br/ufsc/curso/SpringBoot_Postagem/BackEndSpringBootPostagemApplication.java
+++ src/main/java/br/ufsc/curso/SpringBoot_Postagem/BackEndSpringBootPostagemApplication.java
@@ -2,12 +2,21 @@ package br.ufsc.curso.SpringBoot_Postagem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BackEndSpringBootPostagemApplication {
+public class BackEndSpringBootPostagemApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndSpringBootPostagemApplication.class, args);
 	}
+        
+        // Criando um bloqueio para método DELETE
+        public void addCorsMappings(CorsRegistry registry) {                    //esta classe implementa algumas configurações globais CORS
+            
+            registry.addMapping("/usuarios/**").    // todos os endpoints que comecem com usuarios (** - Todos)
+                    allowedMethods("GET", "POST", "PUT");  //   métodos permitidos
+        }
 
 }
