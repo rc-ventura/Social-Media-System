180ef577dfb77c1c32e59dff76e123d76edf7e91
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-12-23 08:38:20 -0300

    Create JWTTokenAutenticacaoService.java

---
+++ src/main/java/br/ufsc/curso/SpringBoot_Postagem/Services/JWTTokenAutenticacaoService.java
@@ -0,0 +1,17 @@
+
+package br.ufsc.curso.SpringBoot_Postagem.Services;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author RC_Ventura
+ */
+@Component
+@Service
+public class JWTTokenAutenticacaoService {
+    
+    
+    
+}
